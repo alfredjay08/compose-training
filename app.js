d89340e818a478ce294f15d02c31202764cb04ac
alfredjay08 <54141766+alfredjay08@users.noreply.github.com>
2022-10-03 14:16:02 +0800

    Currying

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//Composition
+
 const getDoubleOf = (num) => num * 2;
 
 const getHalfOf = (num) => num / 2;
@@ -7,3 +9,15 @@ const mirror = (doubleOf, halfOf) => (n) => halfOf(doubleOf(n));
 const getMirror = mirror(getDoubleOf, getHalfOf);
 
 console.log(getMirror(14));
+
+// Currying
+
+const greet = (name) => (age) => (address) => {
+  return `Hello! My name is ${name}, I am ${age} years old and I lived in ${address}`;
+};
+
+const personToGreet = greet("Alicred Jay");
+const personsAgeToGreet = personToGreet(23);
+const personsAddressToGreet = personsAgeToGreet("Palawan");
+
+console.log(personsAddressToGreet);
